e0e232c24b9e2ff8e05a48bb42f8baa30a242879
Justin Berke <Justin.Berke+github@gmail.com>
2017-04-23 22:50:46 -0400

    Update Lightning.ino
    Code cleanup and header comments

--- Lightning.ino
+++ Lightning.ino
@@ -1,14 +1,16 @@
-/*  Lightning Flash Simulator
+/*  Lightning Flash Simulator:
+      The intent of this project is to add lightning flashes
+      to a pair of cloth clouds that are mounted over my 
+      daughter's crib (see Ikea HIMMELSK - https://goo.gl/1AFNct).
+      Using an Arduino Uno to control two or more LEDs, this program
+      will use random numbers to populate a number of parameters --
+      such as number of of flashes, flash spacing, cloud-to-cloud
+      flash pattern offset, etc. -- to produce a realistic 
+      pattern of lightning flashes inside the clouds.
 */
 
 int pinLED = 13;        // Set onboard LED pin address
-
 int flashDelay = 1000;  // Set delay between flash events
-
-
-void setup() {
-  pinMode(pinLED, OUTPUT);  // Set pin as output
-}
 
 int stormRandomNumber(int lowBound, int highBound) {
   // Placeholder function to return flash paramaters
@@ -29,10 +31,12 @@ void flash() {
   }
 }
 
-void loop() {
-  // Call a flash request
-  flash();
+void setup() {
+  pinMode(pinLED, OUTPUT);  // Set pin as output
+}
 
-  // Pause between flashes
-  delay(flashDelay);
+void loop() {
+  flash(); // Call a flash request
+  
+  delay(flashDelay);   // Pause between flashes
 }
